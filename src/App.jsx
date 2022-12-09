42cb674f889d2093dfb790e30710ac2b52574dd1
Katherine <Evans.Katherine.E@gmail.com>
2022-12-08 18:39:50 -0600

    create recipes variable and pass it to RecipesIndex

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Header() {
   return (
     <div>
@@ -25,22 +27,19 @@ function RecipesNew() {
   );
 }
 
-function RecipesIndex() {
+function RecipesIndex(props) {
+  console.log(props.recipes);
   return (
     <div id="recipes-index">
       <h1>All Recipes</h1>
-      <div className="recipes">
-        <h2>Raw Eggs</h2>
-        <img src="https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg" alt="" />
-        <p>Chef: Peter Jang</p>
-        <button>More Info</button>
-      </div>
-      <div className="recipes">
-        <h2>Mud Pie</h2>
-        <img src="https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg" alt="" />
-        <p>Chef: Jay Wengrow</p>
-        <button>More Info</button>
-      </div>
+      {props.recipes.map((recipe) => (
+        <div key={recipe.id} className="recipes">
+          <h2>{recipe.title}</h2>
+          <img src={recipe.image_url} alt="" />
+          <p>Chef: {recipe.chef}</p>
+          <button>More Info</button>
+        </div>
+      ))}
     </div>
   );
 }
@@ -56,10 +55,31 @@ function Footer() {
 }
 
 function Home() {
+  const [recipes, setRecipes] = useState([
+    {
+      id: 1,
+      title: "Raw Eggs",
+      chef: "Peter Jang",
+      image_url: "https://cdn.britannica.com/94/151894-050-F72A5317/Brown-eggs.jpg",
+    },
+    {
+      id: 2,
+      title: "Mud Pie",
+      chef: "Jay Wengrow",
+      image_url: "https://static.onecms.io/wp-content/uploads/sites/9/2017/12/mud-pie-XL-RECIPE2016.jpg",
+    },
+    {
+      id: 3,
+      title: "Pizza",
+      chef: "Jay Wengrow",
+      image_url:
+        "https://static.onecms.io/wp-content/uploads/sites/9/2021/06/15/mozzarella-pizza-margherita-FT-RECIPE0621.jpg",
+    },
+  ]);
   return (
     <div>
       <RecipesNew />
-      <RecipesIndex />
+      <RecipesIndex recipes={recipes} />
     </div>
   );
 }
