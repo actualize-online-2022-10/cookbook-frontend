bd0bfcf65111b5b9db22b2fdacbfc6b4a2f61410
Katherine <Evans.Katherine.E@gmail.com>
2022-12-18 12:08:22 -0600

    adds filter bar in recipes index

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 export function RecipesIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div id="recipes-index">
       <h1>All Recipes</h1>
-      {props.recipes.map((recipe) => (
-        <div key={recipe.id} className="recipes">
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image_url} alt="" />
-          <p>Chef: {recipe.chef}</p>
-          <button className="btn btn-primary" onClick={() => props.onSelectRecipe(recipe)}>
-            More Info
-          </button>
-        </div>
-      ))}
+      Search recipes:{" "}
+      <input
+        className="form-control"
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+      />
+      {props.recipes
+        .filter((recipe) => recipe.title.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((recipe) => (
+          <div key={recipe.id} className="recipes">
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image_url} alt="" />
+            <p>Chef: {recipe.chef}</p>
+            <button className="btn btn-primary" onClick={() => props.onSelectRecipe(recipe)}>
+              More Info
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
